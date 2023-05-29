d760c7f39971d09d1006be30fead351ad73197ee
Hungken <hungnc051220@gmail.com>
2023-05-30 01:07:28 +0700

    firebase

--- src/renderer/src/layouts/AdminLayout.jsx
+++ src/renderer/src/layouts/AdminLayout.jsx
@@ -24,7 +24,6 @@ const AdminLayout = () => {
   const [toggle, setToggle] = useState(window.innerWidth < 640 ? false : true)
   const { data: dataWaiting } = useGetFoodWaitingQuery({})
   const [updateDevice] = useUpdateDeviceMutation()
-  const [notification, setNotification] = useState({ title: '', body: '' })
   const user = useSelector((state) => state.auth.user)
 
   useEffect(() => {
@@ -38,9 +37,8 @@ const AdminLayout = () => {
 
     // Listen for service successfully started
     window.electron.ipcRenderer.on(NOTIFICATION_SERVICE_STARTED, (_, token) => {
-      console.log('service successfully started', token)
+      console.log('service successfully started', token);
       onUpdateDevice(token)
-      alert(1)
     })
 
     // Handle notification errors
@@ -59,12 +57,50 @@ const AdminLayout = () => {
       if (serverNotificationPayload.notification.body) {
         // payload has a body, so show it to the user
         console.log('display notification', serverNotificationPayload)
-        let myNotification = new Notification(serverNotificationPayload.notification.title, {
-          body: serverNotificationPayload.notification.body
-        })
-        myNotification.onclick = () => {
-          console.log('Notification clicked')
+        // let myNotification = new Notification(serverNotificationPayload.notification.title, {
+        //   body: serverNotificationPayload.notification.body
+        // })
+        const message = JSON.parse(serverNotificationPayload.data.content1)
+        if (message.type === 'order_updated') {
+          dispatch(setUpdate(true))
+          setTimeout(() => dispatch(setUpdate(false)), 300)
         }
+        if (message.type === 'order_paid') {
+          dispatch(setStatusPayment(true))
+          setTimeout(() => dispatch(setStatusPayment(false)), 300)
+        }
+        if (message.type === 'payment_qr_static') {
+          dispatch(setStatusPayment(true))
+          setTimeout(() => dispatch(setStatusPayment(false)), 300)
+        }
+        toast.success(serverNotificationPayload?.notification?.body)
+        console.log('message from firebase', serverNotificationPayload)
+        const result = store.dispatch(apiSlice.endpoints.getFloors.initiate({}))
+        const result1 = store.dispatch(
+          apiSlice.endpoints.getOrders.initiate({
+            pageSize: 10000,
+            orderStatuses: ['WAITING'],
+            web: true
+          })
+        )
+        const result3 = store.dispatch(
+          apiSlice.endpoints.getOrders.initiate({
+            pageSize: 10000,
+            takeAway: true,
+            onTable: false,
+            orderStatuses: ['CREATED']
+          })
+        )
+        const result2 = store.dispatch(
+          apiSlice.endpoints.getNotifications.initiate({
+            pageSize: 20,
+            pageNumber: 0
+          })
+        )
+        result.refetch()
+        result1.refetch()
+        result2.refetch()
+        result3.refetch()
       } else {
         // payload has no body, so consider it silent (and just consider the data portion)
         console.log(
@@ -73,6 +109,11 @@ const AdminLayout = () => {
         )
       }
     })
+
+    // Start service
+    const senderId = '380701649048' // <-- replace with FCM sender ID from FCM web admin under Settings->Cloud Messaging
+    console.log('starting service and registering a client')
+    window.electron.ipcRenderer.send(START_NOTIFICATION_SERVICE, senderId)
   }, [])
 
   // useEffect(() => {
